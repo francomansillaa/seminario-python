556883c02cccbe012da587adeac9c30e248bcda7
francomansillaa <163062318+francomansillaa@users.noreply.github.com>
2024-03-17 09:03:27 -0300

    Ahora el usuario pierde cuando el número de fallos es alcanzado

--- game.py
+++ game.py
@@ -7,9 +7,11 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_fallos = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
+# contador de fallos
+fallos = 0
 
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
@@ -18,19 +20,21 @@ word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+while fallos < max_fallos:
     # Pedir al jugador que ingrese una letrap
     letter = input("Ingresa una letra: ").lower()
 
     # punto 7 solucion de bug de string vacio
     if letter == "":
         print("Valor invalido, ingrese una letra ;)")
+        fallos +=1
         continue
 
     # Verificar si la letra ya ha sido adivinada
 
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
+        fallos +=1
         continue
 
     # Agregar la letra a la lista de letras adivinadas
@@ -41,6 +45,7 @@ for i in range(max_attempts):
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
+        fallos += 1
 
     # Mostrar la palabra parcialmente adivinada
     letters = []
@@ -57,5 +62,5 @@ for i in range(max_attempts):
         print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
         break
 else:
-        print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+        print(f"¡Oh no! Has fallado {max_fallos} veces.")
         print(f"La palabra secreta era: {secret_word}")
